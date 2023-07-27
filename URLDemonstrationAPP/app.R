6214402352ff09fd8c71c931f6b475349e3ef790
Victor Harbo Johnston <vhol@kb.dk>
2023-07-27 08:43:50 +0200

    Update app to display three plots from api get request

--- URLDemonstrationAPP/app.R
+++ URLDemonstrationAPP/app.R
@@ -1,51 +1,56 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(tidyverse)
+library(wesanderson)
+library(urltools)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+# Define UI ----
+ui <- navbarPage(
+  titlePanel('Showcase of Mediestream API'),
+  tabsetPanel(
+    tabPanel("URL",
+             sidebarLayout(
+               sidebarPanel( 
+                 textInput("url", 
+                           p("Url:"), 
+                           value = "")),
+               mainPanel(
+                 tableOutput('familyIdTable'),
+                 tableOutput('lplaceTable'),
+                 plotOutput("plot", click = "plot_click"),
+                 verbatimTextOutput("info"),
+                 textOutput("result"))))
+  )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  output$familyIdTable <- renderTable(
+    read_csv(paste0(input$url)) %>% 
+      count(familyId, sort = TRUE) %>% 
+      top_n(10)
+  )
+  
+  output$lplaceTable <- renderTable(
+    read_csv(paste0(input$url)) %>% 
+      count(lplace, sort = TRUE) %>% 
+      top_n(10)
+  )
+  
+  output$plot <- renderPlot({
+    read_csv(paste0(input$url)) %>% 
+      count(familyId, sort = TRUE) %>% 
+      top_n(20) %>% 
+      ggplot(aes(x = familyId, y = n))+ 
+      geom_col(fill = "#0B775E") +
+      coord_flip() +
+      labs(title = paste0('Amount of articles mentioning \'revolution\''), 
+           subtitle = "dispersed on familyId from 1840 to 1880",
+           x = "familyId")
+  }, res = 96) 
+  
 }
 
-# Run the application 
+# Run the app ----
 shinyApp(ui = ui, server = server)
