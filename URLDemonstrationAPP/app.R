ea16cefd61fb2b2d94ad3bc7a07241305bf2aefc
maxodsbjerg <mape168@gmail.com>
2023-08-10 18:19:47 +0200

    Add bigram network and css styling

--- URLDemonstrationAPP/app.R
+++ URLDemonstrationAPP/app.R
@@ -3,6 +3,8 @@ library(tidyverse)
 library(wesanderson)
 library(urltools)
 library(shinyWidgets)
+library(ggraph)
+library(igraph)
 
 revolutionValue = "http://labs.statsbiblioteket.dk/labsapi/api/aviser/export/fields?query=revolution%20AND%20py%3A%5B1840%20TO%201880%5D&fields=link&fields=recordID&fields=timestamp&fields=pwa&fields=cer&fields=fulltext_org&fields=pageUUID&fields=editionUUID&fields=titleUUID&fields=editionId&fields=familyId&fields=newspaper_page&fields=newspaper_edition&fields=lplace&fields=location_name&fields=location_coordinates&max=1&structure=header&structure=content&format=CSV"
 
@@ -32,15 +34,18 @@ ui <- fluidPage(
                    column(width = 6,
                           tableOutput('lplaceTable'))))
                ),
-           )
-  ),
+           ),
+    tabPanel("Ordnetværk",
+             sliderInput("n_slider", "Bagatelgrænse for antal ordforbindelser", min = 2, max = 20, value = 8), 
+             plotOutput("networkgraph", click = "plot_click")
+  )),
   img(src='DKB_logo_expanded_white_small_RGB.png', align = "bottomleft", width = "15%")
 )
 
 server <- function(input, output) {
-  
+  stopord <- read_csv("https://gist.githubusercontent.com/maxodsbjerg/4d1e3b1081ebba53a8d2c3aae2a1a070/raw/e1f63b4c81c15bb58a54a2f94673c97d75fe6a74/stopord_18.csv")
   data <- eventReactive(input$load, {
-    data <- read_csv(paste0(input$url))
+  data <- read_csv(paste0(input$url))
   })
   
   output$familyIdTable <- renderTable(
@@ -54,6 +59,9 @@ server <- function(input, output) {
       count(lplace, sort = TRUE) %>% 
       top_n(10)
   )
+  
+
+  
   
   output$plot <- renderPlot({
     data() %>% 
@@ -102,7 +110,33 @@ server <- function(input, output) {
                                   angle=0)
         )
   }, res = 96, bg="transparent")
+
+  slider_input <- reactive({
+    input$n_slider
+  }) %>% debounce(1000)
   
+output$networkgraph <- renderPlot({
+  data() %>% 
+    unnest_tokens(bigram, fulltext_org, token = "ngrams", n = 2) %>% 
+    separate(bigram, c("word1", "word2"), sep = " ") %>% 
+    filter(!word1 %in% stopord$word) %>%
+    filter(!word2 %in% stopord$word) %>% 
+    count(word1, word2, sort = TRUE) -> bigrams_count
+  
+  bigram_graph <- bigrams_count %>%
+    filter(n > slider_input()) %>%
+    graph_from_data_frame()
+  
+  a <- grid::arrow(type = "closed", length = unit(.15, "inches"))
+  
+  ggraph(bigram_graph, layout = "fr") +
+    geom_edge_link(aes(edge_alpha = n), show.legend = FALSE,
+                   arrow = a, end_cap = circle(.07, 'inches')) +
+    geom_node_point(color = "#FBFAF5", size = 3) +
+    geom_node_text(aes(label = name), vjust = 1, hjust = 1) +
+    theme_void()
+  
+}, res = 96, bg="transparent")  
 }
 
 # Run the app ----
